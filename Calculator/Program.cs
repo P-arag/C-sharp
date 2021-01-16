548ab953cdf2101cc2fb32a39e5eaac4dca192da
Parag Jyoti Pal <P-arag@github.com>
2021-01-16 14:28:17 +0530

    Initial Commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first operand:- ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nEnter the second operand:- ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nEnter the operator:- ");
+            char myOperator = Convert.ToChar(Console.ReadLine());
+
+            if(myOperator == '+')
+                Console.WriteLine("The Sum is:- "+ (a+b));
+            else if(myOperator == '-')
+                Console.WriteLine("the difference is ")    
+
+        }
+    }
+}
---
+++ Hello_World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!\n");// println vs print
+            Console.WriteLine("Let's learn C#");
+        }
+    }
+}
