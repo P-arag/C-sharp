a58dd25912c86ad3df9dcae096a5ca74027565f6
Parag Jyoti Pal <P-arag@github.com>
2021-01-18 22:07:24 +0530

    Calculator Commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,15 +8,29 @@ namespace Calculator
         {
             Console.Write("Enter the first operand:- ");
             double a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\nEnter the second operand:- ");
+            Console.Write("\nEnter the second operand:- ");
             double b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("\nEnter the operator:- ");
             char myOperator = Convert.ToChar(Console.ReadLine());
 
-            if(myOperator == '+')
-                Console.WriteLine("The Sum is:- "+ (a+b));
-            else if(myOperator == '-')
-                Console.WriteLine("the difference is ")    
+            if (myOperator == '+')
+            {
+                Console.WriteLine("The Sum is:- " + (a + b));
+            }
+            else if (myOperator == '-')
+            {
+                Console.WriteLine("The Difference is:- " + (a - b));
+            }
+            else if (myOperator == '*')
+            {
+                Console.WriteLine("The Product is:- " + (a * b));
+            }
+            else if (myOperator == '/')
+            {
+                Console.WriteLine("The Quotient is:- " + (a / b));
+            }
+
+            Console.ReadKey();
 
         }
     }
